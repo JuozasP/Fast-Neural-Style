b1b5567958bbf6d262fe7810a3e9a2d4ad3f2553
Juozas <PetkelisJuozas@gmail.com>
2018-06-01 10:10:19 +0300

    minor refactor

--- app/src/main/java/com/codebit/fastneuralstyle/MainActivity.kt
+++ app/src/main/java/com/codebit/fastneuralstyle/MainActivity.kt
@@ -9,10 +9,21 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import android.R.attr.data
+import android.net.Uri
+import android.os.Environment
+import android.support.v4.app.NotificationCompat.getExtras
+import android.os.Environment.DIRECTORY_PICTURES
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    val CAMERA_REQUEST_CODE = 0
+    val CAMERA_REQUEST_CODE = 1
+    var mCurrentPhotoPath: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,17 +39,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-
-        when(requestCode){
-            CAMERA_REQUEST_CODE ->{
-                if(resultCode == Activity.RESULT_OK && data != null){
-                    imageView.setImageBitmap(data.extras.get("data") as Bitmap)
-                }
-            }
-            else -> {
-                Toast.makeText(this, "Unrecognised request code", Toast.LENGTH_SHORT).show()
-            }
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            val extras = data?.getExtras()
+            val imageBitmap = extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
         }
+    }
+
+
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir      /* directory */
+        )
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath()
+        return image
+    }
+
+    private fun galleryAddPic() {
+        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+        val f = File(mCurrentPhotoPath)
+        val contentUri = Uri.fromFile(f)
+        mediaScanIntent.data = contentUri
+        this.sendBroadcast(mediaScanIntent)
     }
 }
