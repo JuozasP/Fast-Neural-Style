021af469465ac6d54cd042e746afb7298701a4cf
Juozas <PetkelisJuozas@gmail.com>
2019-06-28 23:19:45 +0300

    version bumps

--- app/src/main/java/com/codebit/fastneuralstyle/MainActivity.kt
+++ app/src/main/java/com/codebit/fastneuralstyle/MainActivity.kt
@@ -10,6 +10,7 @@ import android.provider.MediaStore
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import android.R.attr.data
+import android.app.AlertDialog
 import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
@@ -25,6 +26,9 @@ import java.util.*
 import android.content.Context.MODE_PRIVATE
 import android.content.ContextWrapper
 import java.io.FileOutputStream
+import android.content.DialogInterface
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -50,6 +54,26 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Could not create file!", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    fun selectImage(): Void {
+        val options = arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("Add Photo!");
+        builder.setItems(options) { dialog, item ->
+            if (options[item].equals("Take Photo")) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                val f = File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg")
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f))
+                startActivityForResult(intent, 1)
+            } else if (options[item].equals("Choose from Gallery")) {
+                val intent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent, 2)
+            } else if (options[item].equals("Cancel")) {
+                dialog.dismiss()
+            }
+        }
+        builder.show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
